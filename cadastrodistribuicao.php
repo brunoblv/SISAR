2d5e96ccc8b5c2e329353b2451f5cc0e10d97b47
PMSPSMUL <55211643+brunoblv@users.noreply.github.com>
2023-10-05 15:00:17 -0300

    V 1.0

--- cadastrodistribuicao.php
+++ cadastrodistribuicao.php
@@ -128,7 +128,7 @@ if ($permissao == 2) {
 								$tipoalvara2 = $receber_cadastros['tipoalvara2'];
 								$tipoalvara3 = $receber_cadastros['tipoalvara3'];
 								$stand = $receber_cadastros['stand'];
-								$sts = $receber_cadastros['sts'];								
+								$sts = $receber_cadastros['sts'];
 								$decreto = $receber_cadastros['decreto'];
 								$dataad = $receber_cadastros['dataad'];
 								
